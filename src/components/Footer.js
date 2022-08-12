45eb6861d6c9d7552c230cf3d2147d6403897b21
Magda-GitHub <antoniakmagdalena@wp.pl>
2022-08-12 14:22:32 +0200

    netfixx navigation

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+// import "../components/Footer.scss";
+// function Footer() {
+//   return <footer className="footer">222</footer>(
+//     <div>
+      
+//     </div>
+//   )
+// };
+
+// export default Footer
---
+++ src/components/Navigation.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState} from "react";
+import "../components/Navigation.scss";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
+import {faBell} from '@fortawesome/free-solid-svg-icons';
+import {faCircleInfo} from '@fortawesome/free-solid-svg-icons';
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons';
+
+export default function Navigation() {
+
+
+const [navigationBlack, setNavigationBlack] = useState(false);
+const [toggleMenu, setToggleMenu] = useState(false);
+
+const transitionNav = () => {
+  window.scrollY > 100 ? setNavigationBlack(true) : setNavigationBlack(false);
+};
+
+useState(() => {
+  document.addEventListener("scroll", transitionNav);
+});
+
+
+const handleClick = () => {
+  console.log(toggleMenu);
+  toggleMenu ? setToggleMenu(false) : setToggleMenu(true);
+};
+
+
+  return (
+    <div className={`nav ${navigationBlack || toggleMenu ? "nav-black" :"nav-transparent"} ${toggleMenu && "show"}`}>
+      <button className="nav_burger" onClick={handleClick}>
+      <FontAwesomeIcon icon={faBars}></FontAwesomeIcon></button>
+      <img src="../images/netflix_logo.png" className="nav_logo"alt="netflixx"/>
+      <nav className="nav_links">
+        <a href="/" className ="nav_link">
+        Homepage
+        </a>
+        <a href="/" className ="nav_link">
+        Series
+        </a>
+        <a href="/" className ="nav_link">
+        Movies
+        </a>
+        <a href="/" className ="nav_link">
+        New and Popular
+        </a>
+        <a href="/" className ="nav_link">
+        My list
+        </a>
+        </nav>
+        <div className="nav_rights">
+            <a href="/" className="nav_right">
+            <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+            </a>
+            <a href="/" className="nav_right">
+            KID
+            </a>
+            <a href="/" className="nav_right">
+            <FontAwesomeIcon icon={faBell}></FontAwesomeIcon>
+            </a>
+            <a href="/" className="nav_right">
+            <img src="../images/avatar1.png" className="avatar" alt="avatar"/>
+            <FontAwesomeIcon icon={faCaretDown}></FontAwesomeIcon>
+            </a>
+            <a href="/" className="nav_right">
+            <FontAwesomeIcon icon={faCircleInfo}></FontAwesomeIcon>
+            </a>
+        </div>
+    </div>
+  )
+}
